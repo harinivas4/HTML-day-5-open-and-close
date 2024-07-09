05725094ad366b65ad2affb7fe87fb59693d7e41
Harinivas <chitraharinivas004@gmail.com>
2024-07-09 16:07:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById('open').addEventListener('click',function(){document.getElementById("img").src="https://thumbs.dreamstime.com/b/open-book-isolated-white-16094903.jpg"});
+document.getElementById('close').addEventListener('click',function(){document.getElementById("img").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0HKxW6t_MslfwIJgHCaswrsF3Z43D346k1A&s"});
